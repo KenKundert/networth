ea39a7a111856b7d8a996ad5586b24d189af62ba
Ken Kundert <ken@theKunderts.net>
2020-04-25 12:24:02 -0700

    nit

--- setup.py
+++ setup.py
@@ -41,6 +41,7 @@ setup(
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Topic :: Utilities',
     ],
 )
